d215471d0f96dc2f85d82d2a6a655d731bec0045
Colin Sidoti <colin@clerk.dev>
2023-11-07 19:04:30 -0800

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p>This page is protected.</p>
+        <div>
+          <UserButton />
+        </div>
+      </div>
+    </main>
+  );
+}
